e68cee0f7ebcd21efd9510d8570b1d6c5399f232
Naoki Ikeguchi <root@siketyan.dev>
2021-06-11 19:45:32 +0900

    Fix a function signature

--- keymap.c
+++ keymap.c
@@ -152,7 +152,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
 
-    return true;
+  return true;
 };
 
 bool muse_mode = false;
@@ -161,7 +161,7 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (muse_mode) {
     if (IS_LAYER_ON(_RAISE)) {
       if (clockwise) {
@@ -185,6 +185,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       unregister_code(KC_PGUP);
     }
   }
+
+  return true;
 }
 
 void dip_switch_update_user(uint8_t index, bool active) {
